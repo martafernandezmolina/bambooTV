a8035be9a55cc079a21c246bb5680c1154242766
marta fernandez molina <fernandezmolinamarta@gmail.com>
2020-11-27 13:21:52 +0100

    cambio nombre a BambooTV

---
+++ XE bambooTV/BambooTV/Models/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  TableView-Navigation
+//
+//  Created by Marangi  on 02/11/2020.
+//
+
+import Foundation
+
+struct Item {
+  let name: String
+  let description:String
+  
+}
+
+
